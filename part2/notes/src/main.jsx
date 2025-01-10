5d579260baf31399000efb3074b57bc0914be466
Naima O <naima@example.com>
2025-01-10 15:37:27 +0100

    frond & backend :)

--- part2/notes/src/main.jsx
+++ part2/notes/src/main.jsx
@@ -4,7 +4,9 @@ import "./index.css";
 
 import App from "./App";
 
-axios.get("http://localhost:3001/notes").then((response) => {
+const baseUrl = "http://localhost:3002/notes";
+
+axios.get(baseUrl).then((response) => {
   const notes = response.data;
   console.log(notes);
   ReactDOM.createRoot(document.getElementById("root")).render(
